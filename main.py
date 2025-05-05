5fad64fc5c0276f31dd916df8c02e2fdf58accc0
ShyldWolf <randolfcuello@gmail.com>
2025-05-04 23:06:55 -0400

    add constants, circleshape, player and main

--- main.py
+++ main.py
@@ -3,19 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  screen.fill((0, 0, 0))
-  pygame.display.flip()
+  clock = pygame.time.Clock()
   
-  running = True
-  while running:
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+   
+  while True:
+   dt = clock.tick(60) / 1000
    for event in pygame.event.get():
     if event.type == pygame.QUIT:
         return
+   
+   screen.fill("black")
+   player.update(dt)
+   player.draw(screen)
+   
+   
+   pygame.display.flip()
+   
 
+
+   
         
+   
 if __name__ == "__main__":
    main()
